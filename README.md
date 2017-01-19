c2d2c63e33b3dc677ad72b91ceceb4c319df78d6
Kelvin Kong <kcw88225@gmail.com>
2017-01-19 19:50:09 +0800

    init commit

---
+++ README.md
@@ -0,0 +1,12 @@
+# Fibonacci JS
+
+Fibonacci playground
+
+## Dependencies:
+- jQuery
+- BigInteger
+
+## Examples:
+```javascript
+fibonacci.process(1000) // returns big integer
+```
---
+++ scripts/fibonacci.js
@@ -0,0 +1,45 @@
+(function (root, $) {
+    'use strict';
+    var fibonacci = {},
+        internal = {
+            cache: {}
+        };
+
+    fibonacci.process = function (num) {
+        var result = 0;
+        for (var i = 0; i <= num; i++) {  //starting at leaf node for caching purpose
+            result = calculate(i);
+        }
+
+        return result;
+    }
+
+    /*
+        f(0) = 0
+        f(1) = 1
+        f(n) = f(n-1) + f(n-2)
+    */
+    function calculate(num) {
+        if (internal.cache[num]) {
+            return internal.cache[num];
+        }
+
+        if (num === 0) {
+            return 0;
+        }
+
+        if (num === 1) {
+            return 1;
+        }
+
+        var precedingNum1 = calculate(num - 1),
+            precedingNum2 = calculate(num - 2),
+            bigInteger1 = bigInt(precedingNum1.toString()),
+            bigInteger2 = bigInt(precedingNum2.toString());
+
+        internal.cache[num] = bigInteger1.add(bigInteger2);
+        return internal.cache[num];
+    }
+
+    root.fibonacci = fibonacci;
+})(window, jQuery);
